fbba248aa12a2aa8f8358c58b3f206ad4640271d
Alena Nikulina <alenanikulina0@gmail.com>
2020-05-13 14:26:59 -0400

    added a select material ui field

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,57 +1,72 @@
 import React, { Component } from 'react';
 import TextField from "@material-ui/core/TextField";
-import SelectField from 'material-ui/SelectField';
+import Select from "@material-ui/core/Select";
+// import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import axios from 'axios';
+import ImageResults from '../image-results/ImageResults';
 
 class Search extends Component {
+  state = {
+    searchValue: " ",
+    amount: 15,
+    apiUrl: "https://pixabay.com/api/",
+    apiKey: "16521408-31fe7340522c407055b4dddb9",
+    images: [],
+  };
 
-    state = {
-        searchValue: " ",
-        amount: 15,
-        apiUrl: 'https://pixabay.com/api/',
-        apiKey: '16521408-31fe7340522c407055b4dddb9',
-        images: []
-    };
+  // change state on every key press
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value }, () => {
+      axios
+        .get(
+          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+        )
+        .then((res) => this.setState({ images: res.data.hits }))
+        .catch((err) => console.log(err));
+    });
+  };
 
-    // change state on every key press
-    handleChange = e => {
-        const { value } = e.target;
-        this.setState({ searchValue: value }, () => {
-            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-            .then(res => this.setState({images: res.data.hits}))
-            .catch(err => console.log(err))
-        });
-    };
+//   onAmountChange = (e, index, value) => this.setState({ amount: value });
 
-    render() {
-        console.log(this.state.images)
-        return (
-          <div>
-            <TextField
-              id="outlined-search"
-              label="Search Images"
-              type="search"
-              defaultValue={this.state.searchValue}
-              onChange={this.handleChange}
-              //   fullWidth={true}
-            />
-
-            {/* <SelectField
-                        name="amount"
-                        floatingLabelText="Amount"
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}
-                    >
-                        <MenuItem value={5} primaryText="5" />
-                        <MenuItem value={10} primaryText="10" />
-                        <MenuItem value={15} primaryText="15" />
-                        <MenuItem value={30} primaryText="30" />
-                        <MenuItem value={50} primaryText="50" />
-                    </SelectField> */}
-          </div>
-        );
+    onAmountChange = e => {
+        console.log(e.target)
     }
+
+  render() {
+    console.log(this.state.images);
+    return (
+      <div>
+        <TextField
+          id="outlined-search"
+          label="Search Images"
+          type="search"
+          defaultValue={this.state.searchValue}
+          onChange={this.handleChange}
+          fullWidth={true}
+        />
+        <br></br>
+
+        <Select
+          name="amount"
+          label="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={15}>15</MenuItem>
+          <MenuItem value={20}>20</MenuItem>
+          <MenuItem value={30}>30</MenuItem>   
+        </Select>
+        <br></br>
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default Search
