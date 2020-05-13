86cc73736914b038a4de2cd51cb0ac7f07059368
Alena Nikulina <alenanikulina0@gmail.com>
2020-05-13 15:38:47 -0400

    clearing out the images if nothing is put in the search

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -16,14 +16,18 @@ class Search extends Component {
 
   // change state on every key press
   handleChange = (e) => {
-    const { value } = e.target;
-    this.setState({ searchValue: value }, () => {
-      axios
-        .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchValue}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
-        )
-        .then((res) => this.setState({ images: res.data.hits }))
-        .catch((err) => console.log(err));
+    const val = e.target.value;
+    this.setState({ searchValue: val }, () => {
+        if(val === '') {
+            this.setState({ images: [] });
+        } else {
+            axios
+              .get(
+                `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchValue}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+              )
+              .then((res) => this.setState({ images: res.data.hits }))
+              .catch((err) => console.log(err));
+        }
     });
   };
 
