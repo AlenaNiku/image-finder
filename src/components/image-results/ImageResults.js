6a102fbae873d7353cfbace3e0d4f2e7ef9d984d
Alena Nikulina <alenanikulina0@gmail.com>
2020-05-14 10:54:58 -0400

    added gridtilebar overlay with the info

--- src/components/image-results/ImageResults.js
+++ src/components/image-results/ImageResults.js
@@ -2,53 +2,78 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
-import IconButton from "@material-ui/core/IconButton";
-import ZoomIn from 'material-ui/svg-icons/action/zoom-in';
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 import Dialog from "@material-ui/core/Dialog";
-import FlatButton from 'material-ui/FlatButton';
-import { Grid } from '@material-ui/core';
+import Button from "@material-ui/core/Button";
+import { IconButton } from "@material-ui/core";
+import InfoIcon from "@material-ui/icons/Info";
 
 class ImageResults extends Component {
-    render() {
+  state = {
+    open: false,
+    currentImg: '',
+  };
 
-        let imageListContent;
-        const { images } = this.props;
+  handleOpen = (img) => {
+    this.setState({ open: true, currentImg: img });
+  };
 
-        if (images) {
-          imageListContent = (
-            <GridList cols={3}>
-              {images.map((img) => (
-                <GridListTile
-                  title={img.tags}
-                  key={img.id}
-                  subtitle={
-                    <span>
-                      by <strong>{img.user}</strong>
-                    </span>
-                  }
-                  actionIcon={
-                    <IconButton
-                      onClick={() => this.handleOpen(img.largeImageURL)}
-                    >
-                      <ZoomIn color="white" />
-                    </IconButton>
-                  }
-                >
-                  <img src={img.largeImageURL} alt="" />
-                </GridListTile>
-              ))}
-            </GridList>
-          );
-        } else {
-          imageListContent = null;
-        }
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
-        return (
-            <div>
-                {imageListContent}
-            </div>
-        )
+  render() {
+    let imageListContent;
+    const { images } = this.props;
+
+    if (images) {
+      imageListContent = (
+        <GridList cols={3}>
+          {images.map((img) => (
+            <GridListTile key={img.id}>
+              <img src={img.largeImageURL} alt="" />
+              <GridListTileBar
+                title={img.tags}
+                subtitle={
+                  <span>
+                    by <strong>{img.user}</strong>
+                  </span>
+                }
+                actionIcon={
+                  <IconButton
+                    color="primary"
+                    onClick={() => this.handleOpen(img.largeImageURL)}
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      );
+    } else {
+      imageListContent = null;
     }
+
+    const actions = [
+      <Button label="Close" primary={true} onClick={this.handleClose} />,
+    ];
+
+    return (
+        <div>
+            {imageListContent}
+             <Dialog
+                actions={actions}
+                modal={false}
+                open={this.state.open}
+                onRequestClose={this.handleClose}
+             >
+            <img src={this.state.currentImg} alt="" style={{ width: '100%' }} />
+        </Dialog>
+        </div>
+    );
+  }
 }
 
 ImageResults.propTypes = {
@@ -58,3 +83,5 @@ ImageResults.propTypes = {
 export default ImageResults;
 
 // map through the images, because they are coming in as a prop and it's an array
+
+// GridlistTile needs GridListTileBar to display information about the tile and it will have an overlay
